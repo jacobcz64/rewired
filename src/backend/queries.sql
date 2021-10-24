9f9151c355059f58fcfa11b1a674dcf48b5be3f8
jacobcz <jacobcz@uw.edu>
2021-10-23 22:58:19 -0700

    Implemented node apis for getters

--- src/backend/queries.sql
+++ src/backend/queries.sql
@@ -10,7 +10,7 @@ CREATE TABLE Listing (
     model VARCHAR(255) NOT NULL,
     quantity INT,
     listing_id INT AUTO_INCREMENT PRIMARY KEY,
-    donor_id INT references Donors (id)
+    donor_id INT references Donor (id)
 );
 
 CREATE TABLE Recipient (
@@ -23,11 +23,15 @@ CREATE TABLE Request (
     quantity INT,
     fulfilled BOOLEAN,
     request_id INT AUTO_INCREMENT PRIMARY KEY,
-    donor_id INT references Donors (id)
+    recipient_id INT references Recipient (id)
 );
 
 # Insert dummy data
 INSERT INTO Donor (name) values ('Microsoft'), ('Apple');
+INSERT INTO Listing (brand, device_type, model, quantity, donor_id) values ('Apple', 'tablet', 'iPad Mini 2', 100, 2),
+                                                                           ('Microsoft', 'tablet', 'Surface Pro X', 150, 1);
+INSERT INTO Recipient (name) values ('Roots Young Adult Shelter');
+INSERT INTO Request (device_type, quantity, fulfilled, recipient_id) values ('tablet', 10, false, 1);
 
 # Get Donor
 
--- src/backend/server.js
+++ src/backend/server.js
@@ -10,13 +10,14 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
-app.get('/express_backend', (req, res) => {
+app.get('/test_connection', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
+// Gets list of all donors
 app.get('/donors', (req, res) => {
     connection.query(
-        "SELECT * FROM Donor LIMIT 3",
+        "SELECT * FROM Donor",
         function(error, results) {
             if (error) throw error;
             res.json(results);
@@ -24,4 +25,60 @@ app.get('/donors', (req, res) => {
     );
 });
 
-app.get('/status', (req, res) => res.send('Working!'));
+// Gets list of all recipients
+app.get('/recipients', (req, res) => {
+   connection.query(
+       "SELECT * FROM Recipient",
+       function(error, results) {
+           if (error) throw error;
+           res.json(results);
+       }
+   )
+});
+
+
+// Gets list of all requests associated to recipient id
+// path: /get_requests?id=...
+app.get('/requests', (req, res) => {
+    connection.query(
+        "SELECT * FROM Recipient WHERE id = ?", req.query.id,
+        function(error, results) {
+            if (error) throw error;
+            res.json(results);
+        }
+    )
+});
+
+// Gets list of all listings associated to donor id
+// path: /get_listings?id=...
+app.get('/listings', (req, res) => {
+    connection.query(
+        "SELECT * FROM Donor WHERE id = ?", req.query.id,
+        function(error, results) {
+            if (error) throw error;
+            res.json(results);
+        }
+    )
+});
+
+// Gets information about single donor specified by donor id
+app.get('/donor', (req, res) => {
+    connection.query(
+        "SELECT * FROM Donor WHERE id = ?", req.query.id,
+        function(error, results) {
+            if (error) throw error;
+            res.json(results);
+        }
+    );
+});
+
+// Gets information about single recipient specified by id
+app.get('/recipient', (req, res) => {
+    connection.query(
+        "SELECT * FROM Recipient WHERE id = ?", req.query.id,
+        function(error, results) {
+            if (error) throw error;
+            res.json(results);
+        }
+    );
+});
